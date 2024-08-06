9b016b884610ad123336b568884769bcaf065724
devirz <alireza.dev.zx@gmail.com>
2024-08-06 09:50:18 +0330

    add emojies to plugin list

--- plugins/download.py
+++ plugins/download.py
@@ -5,15 +5,15 @@ async def init(bot):
     @bot.on(events.NewMessage(pattern=r'[Dd][Ll]', outgoing=True))
     async def upload_file(event):
         if event.is_reply:
-            #print(event.stringify())
+            #print(event.message.reply_to_msg)
             replied = await event.get_reply_message()
-            #print("replied: ", replied.stringify())
+            print("replied: ", replied.stringify())
             if not replied.media:
                 await event.edit("**please reply into any media message not text!**")
             else:
                 async def callback(current, total):
                     await event.edit("🚀**Downloading:** `{:.2%}`".format(current / total))
-                    await asyncio.sleep(0.8)
+                    await asyncio.sleep(1)
                 await event.edit("**wait...**")
                 file = await bot.download_media(replied, progress_callback=callback)
                 await event.edit(f"**Downloaded!**\n**⛱File:** `{file}`")
--- plugins/plugins.py
+++ plugins/plugins.py
@@ -1,10 +1,12 @@
 import os
+from random import choice
 from telethon import events
 
 async def init(bot):
     @bot.on(events.NewMessage(pattern=r'plugins', outgoing=True))
     async def get_plugins_list(e):
-        plugins = ["`{}`".format(file.split(".")[0]) for file in os.listdir(os.path.dirname(__file__)) if file[0].isalpha() and file.endswith('.py')]
+        emojies = ["😁","😄","🙄","😀","😋","😎"]
+        plugins = ["{} [`{}`]".format(choice(emojies), file.split(".")[0]) for file in os.listdir(os.path.dirname(__file__)) if file[0].isalpha() and file.endswith('.py')]
         modules = {m.split('.')[-1]: f"__{m}__" for m in plugins}
         print("plugins: ", plugins)
-        await e.edit("**Plugins:**\n{}".format("\n".join(plugins)))
+        await e.edit("**🚀Plugins:**\n{}".format("\n".join(plugins)))
